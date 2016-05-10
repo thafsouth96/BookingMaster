9a0cfde084742b956fdd96d37874eb4027d0fc5e
thafsouth96 <thafsuth.adli@gmail.com>
2016-05-10 11:57:57 +0200

    always trying

--- js/connexion.js
+++ js/connexion.js
@@ -1,26 +1,9 @@
+
 $(document).ready(function() {
 	// Comportement des boutons de menus
 	$('body div #connexion').bind('click', function() { // Au clic sur le bouton connexion"
 
-		$('body').load('html_pages/main.html') ; // On charge la page accueil.html dans le body
+	 	windows.alert("youpi ça fonctionne ") ;
 	});
 
-	$('body nav #inscription').bind('click', function() {
-    // Au clic sur le bouton "mnuQuiSommesNous" dans le menu
-    // On charge la page inscription.html dans le body et on appelle la fonction d'initialisation de cette page
-		$('body').load('pages/inscription.html') ;
-	});
-
-	// On d�clenche un clic sur le bouton "Page 1" pour initialiser le site
-	//$('body nav #mnuAccueil').click();
 });
-
-// ---------------------------------------------------------------------------------------------------------------------------
-// Active une option du menu (l'�l�ment � activer est pass� en param�tre)
-/*function activerOptionMenu($element) {
-	// D�sactive toutes les options du menu (met l'attribut 'actif' � faux)
-	$('body nav input[type="button"]').attr('actif', false);
-	// Active l'option choisie et re�ue en param�tre (met l'attribut 'actif' � vrai)
-	$element.attr('actif', true);
-}
-*/
