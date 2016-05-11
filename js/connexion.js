e42f297d2471d2a45be00e195f68e36799b2dcd6
thafsouth96 <thafsuth.adli@gmail.com>
2016-05-11 19:00:09 +0200

    et youpi

--- js/connexion.js
+++ js/connexion.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 	// Comportement des boutons de menus
 	$('body div #connexion').bind('click', function() { // Au clic sur le bouton connexion"
 
-	 	windows.alert("youpi ça fonctionne ") ;
+	 	alert("youpi ça fonctionne ") ;
+	
 	});
 
 });
