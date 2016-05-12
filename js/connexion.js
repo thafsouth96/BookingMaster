050b7e41bb40e3ccbaeaceacedb2821e543c258c
thafsouth96 <thafsuth.adli@gmail.com>
2016-05-12 17:48:26 +0200

    trying :(

--- js/connexion.js
+++ js/connexion.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
 	// Comportement des boutons de menus
 	$('body div #connexion').bind('click', function() { // Au clic sur le bouton connexion"
 
-	 	alert("youpi ça fonctionne ") ;
-	
+	 	alert("Site en construction") ;
+
 	});
+
+	$('body div #inscription').bind('click', function() { // Au clic sur le bouton connexion"
+
+	 		$('body div').load('../page_html/inscriptionDefault.html') ;
+
+	});
+
 
 });
