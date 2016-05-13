8f6cfc000f64bfb71a949103bd8e9f9b34f5ca3f
Scorsiro <tom.munier@gmail.com>
2016-05-13 14:44:31 +0200

    modif menu

--- MVC/View/menuNav.view.php
+++ MVC/View/menuNav.view.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html>
-  <head>
+
+  <!--head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../View/Design/menuNav.css">
     <title>bookMaster</title>
-  </head>
-  <body>
+  </head-->
+  <div id = "menuNav">
+    <link rel="stylesheet" href="../css/menuNav.css">
       <?php
       global $htmlNav;
       echo $htmlNav;
       ?>
-  </body>
-</html>
+  </div>
