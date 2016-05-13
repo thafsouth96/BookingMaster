2ae2573bf422e613b65fc57b30f458b9f5b11bca
Scorsiro <tom.munier@gmail.com>
2016-05-13 14:58:48 +0200

    encore une

--- View/menuNav.view.php
+++ View/menuNav.view.php
@@ -1,13 +1,15 @@
-
-  <!--head>
+<!--!DOCTYPE html>
+<html>
+  <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../View/Design/menuNav.css">
     <title>bookMaster</title>
   </head-->
   <div id = "menuNav">
-    <link rel="stylesheet" href="../css/menuNav.css">
+      <link rel="stylesheet" href="../css/menuNav.css">
       <?php
       global $htmlNav;
       echo $htmlNav;
       ?>
   </div>
+<!--/html-->
