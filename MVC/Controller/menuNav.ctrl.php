314e77f3f16b8518aa11953e7dff09345802ad76
Scorsiro <tom.munier@gmail.com>
2016-05-13 14:05:51 +0200

    menuNav

--- MVC/Controller/menuNav.ctrl.php
+++ MVC/Controller/menuNav.ctrl.php
@@ -1,11 +1,11 @@
 <?php
     $menu = array(
-    'messagerie'  => array('image'=>'../design/icon/icon_email.png' ,  'text'=>'Messagerie',  'url'=>'../View/mailbox.view.php'),
-    'contacts'  => array('image'=>'../design/icon/contact-book.png',  'text'=>'Mes Contacts',  'url'=>'../View/contact.view.php'),
-    'calendrier' => array('image'=>'../design/icon/datepicker.png',  'text'=>'Mon Calendrier', 'url'=>'../View/calendrier.view.php'),
-    'notes' => array('image'=>'../design/icon/note.png',  'text'=>'Mes Notes', 'url'=>''),
-    'parametres' => array('image'=>'../design/icon/settings.png',  'text'=>'Paramètres', 'url'=>''),
-    'deconnexion' => array('image'=>'../design/icon/logout.png',  'text'=>'Deconnexion', 'url'=>''),
+    'messagerie'  => array('image'=>'../Data/icon/icon_email.png' ,  'text'=>'Messagerie',  'url'=>'../View/mailbox.view.php'),
+    'contacts'  => array('image'=>'../Data/icon/contact-book.png',  'text'=>'Mes Contacts',  'url'=>'../View/contact.view.php'),
+    'calendrier' => array('image'=>'../Data/icon/datepicker.png',  'text'=>'Mon Calendrier', 'url'=>'../View/calendrier.view.php'),
+    'notes' => array('image'=>'../Data/icon/note.png',  'text'=>'Mes Notes', 'url'=>''),
+    'parametres' => array('image'=>'../Data/icon/settings.png',  'text'=>'Paramètres', 'url'=>''),
+    'deconnexion' => array('image'=>'../Data/icon/logout.png',  'text'=>'Deconnexion', 'url'=>''),
     );
 
     function generateMenu($items) {
