1819ab96fc8cbb9de21e3ec841a7b59c99aded0c
thafsouth96 <thafsuth.adli@gmail.com>
2016-05-14 15:15:06 +0200

    et bim ça marche

--- js/loadInscription.js
+++ js/loadInscription.js
@@ -1,4 +1,19 @@
+
 $(document).ready(function() {
 	// Comportement des boutons de menus
-  $('body #content').load('../page_html/inscriptionEtape1.html');
+	$('form #suivant').bind('click', function() { // Au clic sur le bouton "mnuAccueil" dans le menu
+		verifierEtape1();
+
+	});
 });
+
+
+function verifierEtape1() {
+	// Comportement des boutons de menus
+  if ($('#dateNaiss').val() === "") {
+    alert('Saisir la date de naissance');
+    $('#dateNaiss').focus();
+    return false ;
+  }
+$('#content').load('inscriptionEtape2.view.html');
+}
