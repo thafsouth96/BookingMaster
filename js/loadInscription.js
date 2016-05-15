609f185a7ae69d51e8dbd7af3774626846c586de
thafsouth96 <thafsuth.adli@gmail.com>
2016-05-15 12:26:59 +0200

    Etape2 de l'inscription et gérer les champs vide

--- js/loadInscription.js
+++ js/loadInscription.js
@@ -10,10 +10,46 @@ $(document).ready(function() {
 
 function verifierEtape1() {
 	// Comportement des boutons de menus
-  if ($('#dateNaiss').val() === "") {
+	if ($('#nom').val() === ""){
+		alert('Saisir le nom');
+		$('#nom').focus();
+	return false ;
+	 }
+
+	else if ($('#prenom').val() === ""){
+		alert('Saisir prénom');
+		$('#prenom').focus();
+		return false;
+
+	}
+  else if ($('#dateNaiss').val() === "") {
     alert('Saisir la date de naissance');
     $('#dateNaiss').focus();
     return false ;
   }
-$('#content').load('inscriptionEtape2.view.html');
+	else if ($('#adresse').val() === ""){
+		alert('Saisir votre adresse électronique');
+		$('#adresse').focus();
+		return false ;
+
+	}
+	else if ($('#password').val() === "") {
+		alert('Veuillez saisir votre mot de passe');
+		$('#password').focus();
+		return false ;
+
+	}
+
+	else if ($('#passwordConfirm').val() === ""){
+		alert('Veuillez confirmer la saisie de votre mot de passe');
+		$('#passwordConfirm').focus();
+		return false ;
+	}
+	else if ($('#password').val() != $('#passwordConfirm').val()){
+		alert('la confirmation de votre mot de passe ne correspond pas à celui-ci');
+		$('#passwordConfirm').focus();
+		return false ;
+	}
+$('#content').load('../View/InscriptionEtape2.view.html');
+
 }
