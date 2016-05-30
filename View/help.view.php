65666f8850dd2ad2db336c3bcece9821be2c0713
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2016-05-30 13:26:26 +0200

    Update help.view.php

--- View/help.view.php
+++ View/help.view.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="Design/help.css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="../css/help.css" media="screen" charset="utf-8">
     <title>bookMaster</title>
   </head>
   <body>
