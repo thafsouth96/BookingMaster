227e4f8af9b32ea63a73b8901bc9f8db1e89ea5b
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2016-06-01 17:44:21 +0200

    it works

--- View/mailbox-broul.view.php
+++ View/mailbox-broul.view.php
@@ -13,28 +13,12 @@
 
 <div class="mailbox">
   <div class="nav">
-    <a href="mailbox.view.php">Nouveau</a>
-    <a href="mailbox-recu.view.php">Boite de réception</a><!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/mailbox.css" media="screen" charset="utf-8">
-    <title>bookMaster</title>
-  </head>
-  <body>
-
-<header>
-  <h1>messages</h1>
-</header>
-
-<div class="mailbox">
-  <div class="nav">
-    <a href="mailbox.view.php">Nouveau</a>
+    <a href="../View/mailbox.view.php">Nouveau</a>
     <a href="../Controller/mailbox-recu.ctrl.php">Boite de réception</a>
-    <a href="mailbox-inde.view.php">Indésirables</a>
+    <a href="../Controller/mailbox-inde.ctrl.php">Indésirables</a>
     <a href="../Controller/mailbox-envoi.ctrl.php">Envoyé</a>
     <a href="../Controller/mailbox-broul.ctrl.php">Brouillons</a>
-    <a href="mailbox-corbeille.view.php">Corbeille</a>
+    <a href="../View/mailbox-corbeille.view.php">Corbeille</a>
   </div>
   <div class="messages">
     <input name="search" placeholder="search" />
@@ -70,94 +54,6 @@
         //          }
 //
   //              ?>
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">France Gall</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">Psy</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">Jet-li</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">Jackie Chan</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">Truc</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">Machin</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-
-  </div>
-</div>
-</body>
-</html>
-
-    <a href="mailbox-inde.view.php">Indésirables</a>
-    <a href="mailbox-envoi.view.php">Envoyé</a>
-    <a href="mailbox-broul.view.php">Brouillons</a>
-    <a href="mailbox-corbeille.view.php">Corbeille</a>
-  </div>
-  <div class="messages">
-    <input name="search" placeholder="search" />
-    <div class="actions-dropdown">
-      <label>actions <span>▼</span></label>
-      <ul>
-        <li>Ranger</li>
-        <li>Déplacer vers
-          <div class="ssMenu">
-            <ul>
-              <li>Indésirables</li>
-              <li>Corbeille</li>
-            </ul>
-          </div>
-        </li>
-        <li>Supprimer</li>
-      </ul>
-    </div>
-
-
-    <div class="message">
-      <input type="checkbox" />
-      <span class="sender">T-Ara</span>
-      <span class="date">aujourd'hui</span>
-      <span class="title">Question</span>
-    </div>
-    <?php
-
-  //          global $MailBroul;
-
-  //            foreach ($MailBroul as $key => $value) {
-  //                echo "<p>";
-  //                echo $value;
-  //                echo "</p>";
-  //            }
-
-          ?>
     <div class="message">
       <input type="checkbox" />
       <span class="sender">France Gall</span>
